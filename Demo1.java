2e87e1deb0216fda617ed472fe63a96ffab7b5f4
KeerthiVarman2826 <keerthic2826@gmail.com>
2025-08-25 23:53:02 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Demo1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int res = a+b;
+        System.out.println(res);
+    }
+}
