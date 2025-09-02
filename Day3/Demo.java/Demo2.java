612df8ab49c779b6e4fd37d6097f164204298f9a
KeerthiVarman2826 <keerthic2826@gmail.com>
2025-09-02 22:32:58 +0530

    Add files via upload

---
+++ Day3/Demo.java/Demo2.java
@@ -0,0 +1,51 @@
+/*class Demo2{
+    public static void main(String[]args){
+        String[] playlist= new String[3];
+        playlist[0]="song1";
+        System.out.println(playlist[0]);
+        playlist[1]="song2";
+        System.out.println(playlist[1]);
+        playlist[2]="song3";
+        System.out.println(playlist[2
+        
+        
+        
+        
+        ]);
+    }
+
+}*/
+/*class keerthi{
+    public static void main(String[]args){
+        String[] playlist = {"Song 3","Song 1","Song 2"};
+        System.out.println(playlist[0]);
+
+    }
+}*/
+/*import java.util.Scanner;
+class keerthi3{
+    public static void main(String[]args){
+        int[] score=new int[5];
+        Scanner k =new Scanner(System.in);
+         score[0]=k.nextInt();
+          score[1]=k.nextInt();
+           score[2]=k.nextInt();
+            score[3]=k.nextInt();
+             score[4]=k.nextInt();
+             System.out.println(score[0]+score[1]+score[2]+score[3]+score[4]);
+    }
+}*/
+
+import java.util.Scanner;
+
+class karthi2{
+    public static void main(String[]args){
+        Scanner k = new Scanner(System.in);
+        int[] mark=new int[5];
+        
+        for(int i=0;i<5;i++);{
+            mark[0] =k.nextInt();
+        }
+        System.out.println(mark[0]);
+    }
+}
