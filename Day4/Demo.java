2ce6f2e8d3f93a90e7342159eb858ac2004d6e21
KeerthiVarman2826 <keerthic2826@gmail.com>
2025-09-03 23:12:52 +0530

    Add files via upload

---
+++ Day4/Demo.java
@@ -0,0 +1,48 @@
+package Day4;
+
+/*import java.util.Scanner;
+
+class Demo{
+    public static void main(String[]args){
+        Scanner k=new Scanner(System.in);
+        int[] marks=new int[5];
+        for(int i=0;i<=4;i++){
+            marks[i]=k.nextInt();
+
+
+        }
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+        System.out.println(marks[3]);
+        System.out.println(marks[4]);
+    }
+}*/
+/*import java.util.*;
+class demo{
+    public static void main(String[]args){
+        Scanner k=new Scanner(System.in);
+        int[] marks=new int[5];
+        for(int i=0;i<=4;i++){
+            marks[i] =k.nextInt();
+         }
+         for(int i=0;i<=4;i++){
+            System.out.println(marks[i]);
+         }
+
+    }
+}*/
+import java.util.*;
+class demo{
+    public static void main(String[]args){
+        Scanner k=new Scanner(System.in);
+        int[] number=new int[10];
+        for(int i=0;i<=9;i++){
+            number[i]=k.nextInt();
+        }
+        for(int i=0;i<=9;i++){
+            System.out.println(number[i]);
+
+        }
+    }
+}
